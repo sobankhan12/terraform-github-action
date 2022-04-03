9ae149f3e5caad4f86958fda9d961500c790bc67
Soban khan <sobankhan252@gmail.com>
2022-04-03 09:16:33 +0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,10 @@
 terraform {
   # required_version = ">= 1.1.7"
+  backend "s3" {
+    bucket = "soban252s3bucket"
+    key    = "terraformstate"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
